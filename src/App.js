39b97a9ba60bb1237dcbe13bf69540f299ee2c37
Antonio Stassi <stassiant@outlook.it>
2024-12-03 00:58:06 +0100

    Sending messages through react, to other clients

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+    const [messageReceived, setMessageReceived] = useState('');
+    const sendMessage = () => {
+        socket.emit("send_message",  message);
+    };
+
+    useEffect(() => {
+        socket.on("received_message", (data) => {
+            setMessageReceived(data);
+        });
+    }, [socket]);
+
+    return (
+        <div className="App">
+            <input
+                placeholder="Write a message..."
+                onChange={(event) => {
+                    setMessage(event.target.value);
+                }}
+            />
+            <button onClick={sendMessage}>Send</button>
+            <h1> Message </h1>
+            {messageReceived}
+        </div>
+    );
 }
 
 export default App;
